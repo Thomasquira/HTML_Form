4c2bc3b0a8ee006f901166656c2f83372a14c8ca
Thomasquira <thomasquirogudelo@hotmail.com>
2024-11-01 12:40:53 +0100

    Se cambia el estilo y se agregan los requerimentos de la tarea

--- envio.php
+++ envio.php
@@ -1,12 +1,11 @@
 <html>
 <body>
 
-Hola <?php echo $_POST["nombre"]; ?> !<br>
+Hola <?php echo htmlspecialchars($_POST["nombre"]); ?>!<br>
+Tu correo es <?php echo htmlspecialchars($_POST["correo"]); ?><br>
+Has enviado el siguiente mensaje: <p><?php echo nl2br(htmlspecialchars($_POST["mensaje"])); ?></p>
+Tu respuesta a si deseas recibir respuesta: <?php echo htmlspecialchars($_POST["envioEmail"]); ?><br>
+Eres: <?php echo htmlspecialchars($_POST["tipoUsuario"]); ?> 
 
-Tu correo es <?php echo $_POST["correo"]; ?>
-
-Has enviado el siguiente mensaje: <p> <?php echo $_POST["mensaje"]; ?> </p>
-
-Tu respuesta a si deseas recibir respuesta:  <?php echo $_POST["envioEmail"]; ?> 
 </body>
 </html>
